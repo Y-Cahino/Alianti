83ef370a25ed4d59a1f53f143306d5dd5fe41014
cahinosilva.20184 <cahinosilva.20184@E2-9-PC04>
2024-01-09 12:35:19 +0100

    Aggiungere i file di progetto.

---
+++ Alianti/Componenti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alianti
+{
+    public abstract class Componenti
+    {
+        public abstract string des();
+        public abstract double cos();
+        public abstract void Aggiungi(Componenti componenti);
+        public abstract void Rimuovi(Componenti componenti);
+        public abstract Componenti GetFiglio(int i);
+    }
+    public class Aliante:Componenti
+    {
+        private List<Componenti> componenti=new List<Componenti>();
+        public string des(string s)
+        { 
+        }
+    }
+}
