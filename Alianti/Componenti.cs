092600215e31ea8daf9026b8b3126efc0153c35c
cahinosilvadonasicmento.yancarlos.studente@itispaleocapa.it <cahinosilvadonasicmento.yancarlos.studente@itispaleocapa.it>
2024-01-09 12:58:28 +0100

    metodo generic inizializzato

--- Alianti/Componenti.cs
+++ Alianti/Componenti.cs
@@ -6,19 +6,44 @@ using System.Threading.Tasks;
 
 namespace Alianti
 {
-    public abstract class Componenti
+    public abstract class Componenti<T> where T : Componenti<T>
     {
-        public abstract string des();
-        public abstract double cos();
-        public abstract void Aggiungi(Componenti componenti);
-        public abstract void Rimuovi(Componenti componenti);
-        public abstract Componenti GetFiglio(int i);
+
     }
+    
     public class Aliante:Componenti
     {
         private List<Componenti> componenti=new List<Componenti>();
-        public string des(string s)
-        { 
+        public override string des()
+        {
+            var result = "Aliante:\n";
+            foreach(var componente in componenti)
+            {
+                result += componente.des()+"\n";
+            }
+            return result;
+        }
+        public override double cos()
+        {
+            double tot = 0.0;
+            foreach(var componente in componenti)
+            {
+                tot += componente.cos();
+            }
+            return tot;
+        }
+        public override void Aggiungi(Componenti componente)
+        {
+            componenti.Add(componente);
+        }
+        public override void Rimuovi(Componenti componente)
+        {
+            componenti.Remove(componente);
+        }
+        public override Componenti GetFiglio(int i)
+        {
+            if(i>=0&&i<componenti.Count) return componenti[i];
+            return null;
         }
     }
 }
